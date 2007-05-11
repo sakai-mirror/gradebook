cb1c66c26e715515f4253f460b4132a71b295738
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-05-11 11:50:00 +0000

    SAK-9682 resolve merge conflict that happened as a result of 30345
    git-svn-id: https://source.sakaiproject.org/svn/gradebook/branches/sakai_2-3-x@30376 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- app/ui/src/java/org/sakaiproject/tool/gradebook/ui/AssignmentDetailsBean.java
+++ app/ui/src/java/org/sakaiproject/tool/gradebook/ui/AssignmentDetailsBean.java
@@ -100,14 +100,6 @@ public class AssignmentDetailsBean extends EnrollmentTableBean {
 	protected void init() {
 		if (logger.isDebugEnabled()) logger.debug("loadData assignment=" + assignment + ", previousAssignment=" + previousAssignment + ", nextAssignment=" + nextAssignment);
 		if (logger.isDebugEnabled()) logger.debug("isNotValidated()=" + isNotValidated());
-		if (logger.isDebugEnabled()) logger.debug("workInProgress=" + workInProgress);
-		if (workInProgress) {			
-			// Keeping the current form values in memory is a one-shot deal at
-			// present. The next time the user does anything, the form will be
-			// refreshed from the database.
-			workInProgress = false;
-			return;
-		}
 		super.init();
 
         // Clear view state.
