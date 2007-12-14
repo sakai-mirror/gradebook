17c8fa0ffb3aa952d70e43065e85b139b9fe286e
mmmay@indiana.edu <mmmay@indiana.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-12-14 19:50:36 +0000

    svn merge -r 39184:39185 https://source.sakaiproject.org/svn/gradebook/trunk U    service/impl/src/java/org/sakaiproject/tool/gradebook/facades/sections/AuthzSectionsImpl.java in-143-196:~/java/2-5/sakai_2-5-x/gradebook mmmay$ svn log -r 39184:39185 https://source.sakaiproject.org/svn/gradebook/trunk ------------------------------------------------------------------------ r39185 | wagnermr@iupui.edu | 2007-12-13 09:29:44 -0500 (Thu, 13 Dec 2007) | 4 lines
    SAK-12432
    http://bugs.sakaiproject.org/jira/browse/SAK-12432
    Circular dependency between GradebookService and facade Authz
    fixed class cast exception
    ------------------------------------------------------------------------
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/gradebook/branches/sakai_2-5-x@39278 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- service/impl/src/java/org/sakaiproject/tool/gradebook/facades/sections/AuthzSectionsImpl.java
+++ service/impl/src/java/org/sakaiproject/tool/gradebook/facades/sections/AuthzSectionsImpl.java
@@ -36,11 +36,8 @@ import org.sakaiproject.site.api.Group;
 
 import org.sakaiproject.tool.gradebook.facades.Authn;
 import org.sakaiproject.tool.gradebook.facades.Authz;
-import org.sakaiproject.service.gradebook.shared.Assignment;
 import org.sakaiproject.service.gradebook.shared.GradebookPermissionService;
 import org.sakaiproject.service.gradebook.shared.GradebookService;
-
-import org.sakaiproject.tool.gradebook.Gradebook;
 
 /**
  * An implementation of Gradebook-specific authorization needs based
@@ -330,9 +327,16 @@ public class AuthzSectionsImpl implements Authz {
 			HashMap assignFunctionMap = new HashMap();
 			if (!allGbItems.isEmpty()) {
 				for (Iterator assignIter = allGbItems.iterator(); assignIter.hasNext();) {
-					Assignment assign = (Assignment) assignIter.next();
-					if (assign != null)
-						assignFunctionMap.put(assign.getId(), GradebookService.gradePermission);
+					Object assign = assignIter.next();
+					Long assignId = null;
+					if (assign instanceof org.sakaiproject.service.gradebook.shared.Assignment) {
+						assignId = ((org.sakaiproject.service.gradebook.shared.Assignment)assign).getId();
+					} else if (assign instanceof org.sakaiproject.tool.gradebook.Assignment) {
+						assignId = ((org.sakaiproject.tool.gradebook.Assignment)assign).getId();
+					}
+
+					if (assignId != null)
+						assignFunctionMap.put(assignId, GradebookService.gradePermission);
 				}
 			}
 			
@@ -434,9 +438,16 @@ public class AuthzSectionsImpl implements Authz {
 						Map itemFunctionMap = new HashMap();
 						if (!allGbItems.isEmpty()) {
 							for (Iterator itemIter = allGbItems.iterator(); itemIter.hasNext();) {
-								Assignment item = (Assignment)itemIter.next();
-								if (item != null) {
-									itemFunctionMap.put(item.getId(), GradebookService.gradePermission);
+								Object assign = itemIter.next();
+								Long assignId = null;
+								if (assign instanceof org.sakaiproject.service.gradebook.shared.Assignment) {
+									assignId = ((org.sakaiproject.service.gradebook.shared.Assignment)assign).getId();
+								} else if (assign instanceof org.sakaiproject.tool.gradebook.Assignment) {
+									assignId = ((org.sakaiproject.tool.gradebook.Assignment)assign).getId();
+								}
+
+								if (assignId != null) {
+									itemFunctionMap.put(assignId, GradebookService.gradePermission);
 								}
 							}
 						}
